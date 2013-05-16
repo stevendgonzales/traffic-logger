8fbaf0a69397aeab60765f69fae1d6e18af9995b
Steven Gonzales <stevendgonzales@gmail.com>
2013-05-15 22:21:54 -0500

    restructuring, added data access methods and zip code lookup

--- traffic_logging/__init__.py
+++ traffic_logging/__init__.py
@@ -4,4 +4,3 @@ from sqlalchemy.orm import sessionmaker
 #initialize the application's database engine and create a sessionmaker
 engine = create_engine('sqlite:///traffic.db', echo=True)
 Session = sessionmaker(bind=engine)
-
--- traffic_logging/data/traffic.py
+++ traffic_logging/data/traffic.py
@@ -1,5 +1,3 @@
-from datetime import date, datetime, timedelta
-
 from sqlalchemy import Column, Date, Integer, String, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -50,36 +48,9 @@ class TrafficEvent(Base):
 Base.metadata.create_all(engine)
 
 
-def parse_traffic_event(traffic_item):
-    """
-    function parses the text for a traffic item and
-    creates a new TrafficEvent object
-    """
-    traffic_event = TrafficEvent(
-        problem_nature=traffic_item[0].get_text(),
-        address=traffic_item[1].get_text(),
-        time=traffic_item[2].get_text(),
-        date=get_traffic_event_date(traffic_item[2].get_text()),
-        status=traffic_item[3].get_text(),
-        dispatch_channel=traffic_item[4].get_text()
-    )
-    return traffic_event
 
 
-def get_traffic_event_date(event_time):
-    """
-    Function analyzes the traffic event to determine the date of the event.
-    This is necessary since the source of traffic events only includes the time
-    """
-    event_hour = int(event_time[0:2])
-    now = datetime.now()
 
-    #set the date to be today's date
-    event_date = date.today()
 
-    #if the current tome is between 12AM and 3AM, but
-    #the event_time is between 9PM and 12AM, then set the date for yesterday
-    if now.hour < 3 and event_hour > 21:
-        event_date = date.today()-timedelta(days=1)
 
-    return event_date
+
